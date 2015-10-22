637f6566d99ef0b47a238b3b1ab7c8e55a99467c
Ewan-Brown <ewanbrown2000@gmail.com>
2015-10-22 17:41:57 -0400

    Added More Monster Names
    Added 20 names. Total of 38 names now!

--- ffGame/entities/EntityMonster.java
+++ ffGame/entities/EntityMonster.java
@@ -13,7 +13,7 @@ public class EntityMonster extends EntityBase {
 	static int baseHealth = entities.EntityStats.MONSTER_HEALTH ;
 	static int baseDamage = entities.EntityStats.MONSTER_DAMAGE;
 	static int baseDamageRange = entities.EntityStats.MONSTER_DAMAGERANGE;
-	static String[] MONSTER_NAMES = {"Zombie","Skeleton","Wraith","Bandit","Demon","Platypus","Slime","Cyclops","Dragon","Ogre","Vampire","WereWolf","Unicorn","Ghost","Giant Spider","Deep-Space Kraken","Wendigo","The Thing"};
+	static String[] MONSTER_NAMES = {"Zombie","Skeleton","Wraith","Bandit","Demon","Platypus","Slime","Cyclops","Dragon","Ogre","Vampire","WereWolf","Unicorn","Ghost","Giant Spider","Deep-Space Kraken","Wendigo","The Thing","Satanic Cultist","Goblin","Weeaboo","WHATARETHOOOSE?!?","Ninja Pirate","Slenderman","Rabid Butterfly","The Blob","Giant","Orc","999","Bat","Pokeman","Alien","Barbarian","Bee Swarm","Scorpion","Wolf Pack","Rogue Wizard","ERROR 404"};
 
 	public EntityMonster(String name,Difficulty difficulty) {
 		  super(name,difficulty.mult(baseHealth),difficulty.mult(baseDamage), baseDamageRange);
