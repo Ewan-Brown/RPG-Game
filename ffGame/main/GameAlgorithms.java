a038f0d50fbc90d9402a74db0937dd26d276f469
Ewan-Brown <ewanbrown2000@gmail.com>
2015-10-27 15:29:20 -0400

    Revert "Deleted Old method"
    This reverts commit bb5c71eec1f4b63b29c789863dcf14c81dfc65c9.

--- ffGame/main/GameAlgorithms.java
+++ ffGame/main/GameAlgorithms.java
@@ -42,6 +42,15 @@ public class GameAlgorithms {
 			}
 		}
 	}
+	public static boolean didMiss(){
+		double a = Math.random();
+		if (a < 0.2){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
 	public static int randomRangedInt(int range){
 		//double between -1 and +1
 		double rand = (Math.random() - 0.5) * 2;
